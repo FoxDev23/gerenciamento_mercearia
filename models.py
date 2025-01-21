a0dcb474c84769bb793c3e1078f9cb39fc0cc1b9
FoxDev23 <stanleyfernandoads@gmail.com>
2025-01-20 22:52:08 -0300

    Finalização da models

---
+++ models.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+
+
+
+#Definindo as classes da aplicação...
+
+class Categoria:
+    def __init__(self, categoria):
+        self.categoria = categoria
+
+class Produtos:
+    def __init__(self, nome, preco, categoria):
+        self.nome = nome
+        self.preco = preco
+        self.categoria = categoria
+
+#No caso abaixo, descrevi que "produto" é uma variável do tipo "Produtos", para referenciar do que se trata.
+
+class Estoque:
+    def __init__(self, produto: Produtos, quantidade):
+        self.produto = produto
+        self.quantidade = quantidade
+
+#Como no caso do Estoque, aqui também foi preciso referenciar o "itensVendidos" para identificar que ele é um elemento da classe Produtos.
+class Vendas:
+    def __init__(self, itensVendidos: Produtos, vendedor, comprador, quantidadeVendida, data = datetime.now()):
+        self.itensVendidos = itensVendidos
+        self.vendedor = vendedor
+        self.comprador = comprador
+        self.quantidadeVendida = quantidadeVendida
+
+#Função que registra a data, importada da biblioteca.
+        self.data = data
+        
+class Fornecedor:
+    def __init__(self, nome, cnpj, telefone, categoria):
+        self.nome = nome
+        self.cnpj = cnpj
+        self.telefone = telefone
+        self.categoria = categoria
+
+class Pessoa:
+    def __init__(self, nome, telefone, cpf, email, endereco):
+        self.nome = nome
+        self.telefone = telefone
+        self.cpf = cpf
+        self.email = email
+        self.endereco = endereco
+
+class Funcionario(Pessoa):
+    def __init__(self, clt, nome, telefone, cpf, email, endereco):
+        self.clt = clt
+        super(Funcionario, self).__init__(nome, telefone, cpf, email, endereco)
